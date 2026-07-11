c07c5f142350cd66431df8127a72188f7cab7cc4
PhucThanh <151224911+PhucBeta@users.noreply.github.com>
2026-07-11 10:28:16 +0700

    Update portfolio for deployment

---
+++ app/components/Contact.jsx
@@ -0,0 +1,101 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '../../assets/assets'
+import { useState } from 'react'
+import { motion } from "motion/react"
+
+
+const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "eeb857cd-5daf-45d4-adea-bfaae3ca625c");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
+    return (
+        <motion.div
+            id='contact'
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className='w-full px-[12%] py-20 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
+            <motion.h4
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+                className='text-center mb-2 text-lg font-ovo'>Connect with me</motion.h4>
+            <motion.h2
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.25 }}
+                className='text-5xl text-center font-ovo' > Get in touch</motion.h2 >
+
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.7 }}
+                className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
+                If you’re looking for a dedicated developer who brings creativity,
+                technical skill, and a problem-solving mindset to every project, let’s talk. I’m excited to hear about your ideas and discuss how I can help you bring them to life.
+            </motion.p>
+
+            <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0 }}
+                    className='grid grid-cols-auto gap-6 mt-10 mb-8'>
+
+                    <motion.input type="text" placeholder='Enter your name' required
+                        initial={{ opacity: 0, x: -50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.6, delay: 0.2 }}
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
+                        name='name' />
+
+                    <motion.input type="email" placeholder='Enter your email' required
+                        initial={{ opacity: 0, x: 50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.6, delay: 0.2 }}
+                        className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
+                        name='email' />
+
+                </motion.div>
+                <motion.textarea rows='6' placeholder='Enter your massage' required
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, delay: 0.2 }}
+                    className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90 mb-6'
+                    name='message'></motion.textarea>
+                <motion.button type='submit'
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.3 }}
+                    className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'>Submit now<Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.button>
+                <p className='mt-4'>{result}</p>
+            </form>
+        </motion.div>
+    )
+}
+
+export default Contact
---
+++ app/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '../../assets/assets'
+const Footer = ({ isDarkMode }) => {
+    return (
+        <div className='mt-20'>
+            <div className='text-center'>
+                <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2' />
+
+                <div className='w-max flex items-center gap-2 mx-auto'>
+                    <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6' />
+                    masterphuc01@gmail.com
+                </div>
+            </div>
+
+            <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+                <p>
+                    © 2026 Ciantic. All rights reserved.
+                </p>
+                <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                    <li><a target='_blank' href="https://www.facebook.com/Ciantic">FaceBook</a></li>
+                    <li><a target='_blank' href="https://www.linkedin.com/in/ph%C3%BAc-th%C3%A0nh-8482403a5/">LinkedIn</a></li>
+                    <li><a target='_blank' href="https://github.com/PhucBeta">Github</a></li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
